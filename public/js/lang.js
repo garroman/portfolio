ef3087c4fe7de668526d4851ec836eb2bf880b9b
Roman Lopez Garro (d) <garrodev18@gmail.com>
2021-06-01 19:17:03 -0300

    fixing lang pt. 2

--- public/js/lang.js
+++ public/js/lang.js
@@ -1,22 +1,19 @@
 
 window.addEventListener("load", () => {
-    // LANGUAGE STUFF
+
     const activeColor = "#B16286";
-    let isInSpanish = false;
     const title = document.querySelector("title");
+    const spanishSwitch = document.getElementById("spanish-switch");
+    const englishSwitch = document.getElementById("english-switch");
+
     if (title.classList.contains("en")) {
-      isInSpanish = false;
-    } else {
-      isInSpanish = true;
-    }
 
-    
-    let spanishSwitch = document.getElementById("spanish-switch");
-    let englishSwitch = document.getElementById("english-switch");
-
-    if(isInSpanish) {
-      spanishSwitch.style.color = activeColor; }
-    else {
       englishSwitch.style.color = activeColor;
+
+    } else if (title.classList.contains("es")) {
+
+      spanishSwitch.style.color = activeColor;
+
     }
+
 });
