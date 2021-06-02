8696f3e900bc3d09ba2ee3aca179866df9680e5d
Roman Lopez Garro (d) <garrodev18@gmail.com>
2021-06-02 12:06:50 -0300

    changed 'burger menu' style

--- public/js/light.js
+++ public/js/light.js
@@ -1,11 +1,32 @@
 window.addEventListener("load", () => {
 
-  const lightSwitches = document.querySelectorAll(".light-switch");
+  // Check which theme to apply when page loads
+  const sun = document.getElementById("sun");
+  const moon = document.getElementById("moon");
 
+  if(sun.classList.contains("activated")) {
+
+    setColors(sun, false);
+
+  } else {
+
+    setColors(moon, true);
+
+  }
+
+  // When clicking on the buttons after loading the page
+  const lightSwitches = document.querySelectorAll(".light-switch");
   lightSwitches.forEach(element =>{
 
     element.addEventListener("click", () => {
-      switchTheme(element);
+
+      // Switch theme only if element is not activated
+      const elementIsActive = element.classList.contains("activated");
+
+      if (!elementIsActive) {
+        switchTheme(element);
+      }
+
     });
 
   });
@@ -14,26 +35,41 @@ window.addEventListener("load", () => {
 
 function switchTheme(element) {
 
+      console.log("entered switchTheme()");
       const sun = document.getElementById("sun");
       const moon = document.getElementById("moon");
-      const body = document.querySelector("body");
 
-      if(!element.classList.contains("activated")) { 
 
-        let elId = element.id;
-        element.classList.add("activated");
-        if(elId == "moon") {
-          sun.classList.remove("activated");
-          body.style.setProperty("--bg", "#1d2021"); 
-          body.style.setProperty("--nav-bg", "#000"); 
-          body.style.setProperty("--press-h2-fg", "#fff"); 
+      let elId = element.id;
+      element.classList.add("activated");
 
-        } else {
-          moon.classList.remove("activated");
-          body.style.setProperty("--bg", "#eee"); 
-          body.style.setProperty("--nav-bg", "#fff"); 
-          body.style.setProperty("--press-h2-fg", "#444"); 
-        }
+      if(elId == "moon") {
+sun.classList.remove("activated");
+        setColors(moon, true);
+
+      } else {
+
+        moon.classList.remove("activated");
+        setColors(sun, false);
+
       }
 
 }
+
+function setColors(element, toDarkColors = null) {
+
+  const body = document.querySelector("body");
+  if(toDarkColors) {
+
+      body.style.setProperty("--bg", "#1d2021"); 
+      body.style.setProperty("--nav-bg", "#000"); 
+      body.style.setProperty("--press-h2-fg", "#fff"); 
+
+  } else if (!toDarkColors){
+
+      body.style.setProperty("--bg", "#eee"); 
+      body.style.setProperty("--nav-bg", "#fff"); 
+      body.style.setProperty("--press-h2-fg", "#444"); 
+  }
+}
+
--- public/js/nav.js
+++ public/js/nav.js
@@ -1,26 +1,66 @@
 window.addEventListener("load", () => {
   
+
+    const smallNav = document.querySelector(".smaller-nav");
+    const nav = document.querySelector(".nav-links");
+    const smallNavLi = document.querySelectorAll(".smaller-a");
+    const navLi = document.querySelectorAll(".nav-a");
+
+
+    if (window.innerWidth <= 768) {
+          removeDataPageAttribute(navLi);
+          setDataPageAttribute(smallNavLi);
+    } else {
+        removeDataPageAttribute(smallNavLi);
+        setDataPageAttribute(navLi);
+    }
+
+    window.addEventListener("resize", () => {
+
+        const windowWidth = window.innerWidth;
+
+        if (windowWidth >= 768) {
+
+          removeDataPageAttribute(smallNavLi);
+          setDataPageAttribute(navLi);
+
+          smallNav.style.display = "none";
+        }
+
+        if (windowWidth <= 768) {
+
+          removeDataPageAttribute(navLi);
+          setDataPageAttribute(smallNavLi);
+
+        }
+
+    });
+
     slider();
-    pager();
-  
+    navPager(); 
 
 });
+
 
 let slider = () => {
 
     const menu = document.querySelector(".menu"); 
-    const nav  = document.querySelector(".nav-links");
-
-    const navLinks = document.querySelectorAll(".nav-links li");
-
-
+    const smallerNav  = document.querySelector(".smaller-nav");
+    const smallNavLinks = document.querySelectorAll(".smaller-nav ul li");
+    
     menu.addEventListener("click", () => {
       
       // Toggle nav
-      nav.classList.toggle("active");
+      const display = smallerNav.style.display;
+
+      if (display == "none") {
+        smallerNav.style.display = "flex";
+      } else {
+        smallerNav.style.display = "none";
+      }
 
       // Toggle animation
-      navLinks.forEach((link, index) => {
+      smallNavLinks.forEach((link, index) => {
         if(link.style.animation) {
 
           link.style.animation = '';
@@ -38,19 +78,33 @@ let slider = () => {
     });
 }
 
-const pager = () => {
+const navPager = () => {
     // Section check
     const sections = document.querySelectorAll("section");
     const options = {
       threshold: 0.8
     };
+
     let observer = new IntersectionObserver(navCheck, options);
+
 
     function navCheck(entries) {
       entries.forEach(entry => {
         const entryId = entry.target.id;
         const activeAnchor = document.querySelector(`[data-page=${entryId}]`);
-        const allAnchors = document.getElementsByClassName("nav-a");
+        let allAnchors = ""; 
+        
+        // Check which "nav" set active to
+        if(document.querySelector(".nav-a").hasAttribute("data-page")) {
+
+          allAnchors = document.querySelectorAll(".nav-a");
+
+        } else {
+
+          allAnchors = document.querySelectorAll(".smaller-a");
+
+        }
+
         if(entry.isIntersecting) {
           for(let i = 0; i < allAnchors.length; i++) {
             allAnchors[i].classList.remove("active");
@@ -62,5 +116,18 @@ const pager = () => {
 
     sections.forEach(section => {
       observer.observe(section);
+    });
+}
+
+function removeDataPageAttribute(li) {
+    li.forEach((link,index) => {
+        link.removeAttribute("data-page");
+    });
+}
+
+function setDataPageAttribute(li) {
+    li.forEach((link,index) => {
+        let href = link.hash.split("#")[1];
+        link.setAttribute("data-page", href);
     });
 }
